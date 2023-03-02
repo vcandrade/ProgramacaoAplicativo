10ae4636567c0c17e90c63c57041fdd4d8d09d8d
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2023-03-02 15:20:25 -0300

    added class 01

---
+++ src/aula01/Carro.java
@@ -0,0 +1,44 @@
+package aula01;
+
+public class Carro {
+
+	// atributos
+	String modelo;
+	String marca;
+	int ano;
+	String placa;
+	String cor;
+	
+	// método construtor
+	public Carro(String modelo, String marca, int ano, String placa, String cor) {
+		
+		this.modelo = modelo;
+		this.marca = marca;
+		this.ano = ano;
+		this.placa = placa;
+		this.cor = cor;
+	}
+	
+	// métodos
+	public void acelerar() {
+		
+	}
+	
+	public void frear() {
+		
+	}
+	
+	public void ligar() {
+		
+	}
+	
+	public void desligar() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Carro [modelo=" + modelo + ", marca=" + marca + ", ano=" + ano + ", placa=" + placa + ", cor=" + cor
+				+ "]";
+	}
+}
---
+++ src/aula01/CarroTeste.java
@@ -0,0 +1,15 @@
+package aula01;
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+		
+		Carro carro1 = new Carro("Gol", "Volks", 2022, "ABC-1234", "Vermelho");
+		Carro carro2 = new Carro("Onix", "Chevrolet", 2015, "MNO-9876", "Preto");
+		Carro carro3 = new Carro("Fiesta", "Ford", 2018, "XYZ-6543", "Verde");
+		
+		System.out.println(carro1);
+		System.out.println(carro2);
+		System.out.println(carro3);
+	}
+}
